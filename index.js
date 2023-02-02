1f005c1f76cec8499388bc237d5b5f4e474760da
Jackson <jacksonsukkar91@gmail.com>
2023-02-02 18:39:37 -0500

    commit tree 934e74006d4a3414ae9207dc12a4e5768d228d93

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-function whisper(string) {
+function shout(string){
+    return string.toUpperCase();
+}
+function whisper(string){
     return string.toLowerCase();
 }
 function logShout(string){
-    console.log(shout(string))
+    console.log(shout(string));
 }
 function logWhisper(string){
     console.log(whisper(string))
 }
 function sayHiToHeadphonedRoommate(string){
-     if (string===string.toLowerCase())
-     return "I can't hear you!"
-     if (string===string.toUpperCase())
-     return "YES INDEED!"
-     if (string==="Let's have dinner together!")
-     return "I would love to!"
+    if (string === string.toLowerCase())
+    return "I can't hear you!"
+    if (string === string.toUpperCase())
+    return "YES INDEED!"
+    if (string === "Let's have dinner together!")
+    return "I would love to!"
 }
